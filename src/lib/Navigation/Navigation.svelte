312bf8877ff22911283a2b4947154a9a71e8df20
Austin Barwick <austin.barwick@hey.com>
2024-04-16 19:47:59 -0700

    Add login/register modal

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -1,19 +1,48 @@
 <script lang="ts">
-    import { getDrawerStore } from "@skeletonlabs/skeleton";
+    import {
+        getDrawerStore,
+        getModalStore,
+        type ModalSettings,
+    } from "@skeletonlabs/skeleton";
 
+    const modalStore = getModalStore();
     const drawerStore = getDrawerStore();
+
+    const modal: ModalSettings = {
+        type: "component",
+        component: "AuthModal",
+    };
 
     function closeDrawer(): void {
         drawerStore.close();
     }
+
+    const openAuthModal = (): void => {
+        modalStore.trigger(modal);
+        closeDrawer();
+    };
 </script>
 
 <nav class="list-nav p-4">
     <ul>
         <li><a href="/" on:click={closeDrawer}>🏠 Home</a></li>
-        <li><a href="/plan" on:click={closeDrawer}>✏️ Plan</a></li>
-        <li><a href="/record" on:click={closeDrawer}>🎙️ Record</a></li>
-        <li><a href="/edit" on:click={closeDrawer}>✂️ Edit</a></li>
-        <li><a href="/publish" on:click={closeDrawer}>🎉 Publish</a></li>
+        <li>
+            <a href="/plan" on:click={closeDrawer}>✏️ Plan</a>
+        </li>
+        <li>
+            <a href="/record" on:click={closeDrawer}>🎙️ Record</a>
+        </li>
+        <li>
+            <a href="/edit" on:click={closeDrawer}>✂️ Edit</a>
+        </li>
+        <li>
+            <a href="/publish" on:click={closeDrawer}>🎉 Publish</a>
+        </li>
+        <li>
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="#" on:click|preventDefault={openAuthModal}
+                >🔓 Login/Register</a
+            >
+        </li>
     </ul>
 </nav>
---
+++ src/lib/modals/AuthModal.svelte
@@ -0,0 +1,118 @@
+<script lang="ts">
+    import type { SvelteComponent } from "svelte";
+    import { Tab, TabGroup, getModalStore } from "@skeletonlabs/skeleton";
+
+    const modalStore = getModalStore();
+
+    let tabSet = 0;
+
+    function onLoginSubmit(): void {
+        // if ($modalStore[0].response) {
+        //     $modalStore[0].response(formData);
+        // }
+        modalStore.close();
+    }
+
+    function onRegisterSubmit(): void {
+        // if ($modalStore[0].response) {
+        //     $modalStore[0].response(formData);
+        // }
+        modalStore.close();
+    }
+</script>
+
+{#if $modalStore[0]}
+    <div class="modal-example-form card p-4 w-modal shadow-xl space-y-4">
+        <TabGroup justify="justify-center">
+            <Tab bind:group={tabSet} name="tab1" value={0}>Login</Tab>
+            <Tab bind:group={tabSet} name="tab2" value={1}>Register</Tab>
+            <svelte:fragment slot="panel">
+                {#if tabSet === 0}
+                    <div class="card p-4">
+                        <form class="mt-4">
+                            <div class="mb-6">
+                                <label for="email" class="label mb-2"
+                                    >Email</label
+                                >
+                                <input
+                                    type="email"
+                                    id="email"
+                                    class="input"
+                                    placeholder="Enter your email"
+                                />
+                            </div>
+                            <div class="mb-6">
+                                <label for="password" class="label mb-2">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    class="input"
+                                    placeholder="Enter your password"
+                                />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <button
+                                    class="btn variant-filled"
+                                    type="button"
+                                    on:click={onLoginSubmit}
+                                >
+                                    Sign In
+                                </button>
+                                <a href="/forgotpassword"> Forgot Password? </a>
+                            </div>
+                        </form>
+                    </div>
+                {:else if tabSet === 1}
+                    <div class="card p-4">
+                        <form class="mt-4">
+                            <div class="mb-6">
+                                <label for="email" class="label mb-2">
+                                    Name
+                                </label>
+                                <input
+                                    type="text"
+                                    id="name"
+                                    class="input"
+                                    placeholder="Enter your name"
+                                />
+                            </div>
+                            <div class="mb-6">
+                                <label for="email" class="label mb-2"
+                                    >Email</label
+                                >
+                                <input
+                                    type="email"
+                                    id="email"
+                                    class="input"
+                                    placeholder="Enter your email"
+                                />
+                            </div>
+                            <div class="mb-6">
+                                <label for="password" class="label mb-2">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    class="input"
+                                    placeholder="Enter your password"
+                                />
+                            </div>
+                            <div class="flex items-center justify-between">
+                                <button
+                                    class="btn variant-filled"
+                                    type="button"
+                                    on:click={onRegisterSubmit}
+                                >
+                                    Sign In
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                {/if}
+            </svelte:fragment>
+        </TabGroup>
+    </div>
+{/if}
