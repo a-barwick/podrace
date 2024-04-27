89997dcef130e4a9c9cc5950503541f8208acde8
Austin Barwick <austin.barwick@hey.com>
2024-04-27 10:01:20 -0700

    try to fix caprover deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,12 @@
-import { sentrySvelteKit } from "@sentry/sveltekit";
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [sentrySvelteKit({
-        sourceMapsUploadOptions: {
-            org: "austin-barwick",
-            project: "javascript-sveltekit"
-        }
-    }), sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+    plugins: [sveltekit()],
+    server: {
+        port: 80,
+    },
+    test: {
+        include: ["src/**/*.{test,spec}.{js,ts}"],
+    },
 });
